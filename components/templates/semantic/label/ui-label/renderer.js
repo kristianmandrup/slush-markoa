394d46140b12ed272332dadd3cedfa4d1dcee58c
Kristian Mandrup <kmandrup@gmail.com>
2015-08-17 21:15:11 +0200

    group label

--- components/templates/semantic/label/ui-label/renderer.js
+++ components/templates/semantic/label/ui-label/renderer.js
@@ -3,6 +3,8 @@ var template = require('./template.marko');
 exports.renderer = function(input, out) {
     input.isTag = input.tag ? 'tag' : '';
     input.isRibbon = input.ribbon ? 'ribbon' : '';
-    input.isAttached = input.attached ? 'attached' : '';    
+    input.isAttached = input.attached ? 'attached' : '';
+    input.float = input.float ? 'floating' : '';
+    input.circular = input.circular ? 'circular' : '';
     template.render(input, out);
 };
