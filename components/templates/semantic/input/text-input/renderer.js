6ba313ae3c3a63343b298f9013581f90db7e07ba
Kristian Mandrup <kmandrup@gmail.com>
2015-08-17 18:21:37 +0200

    text input

--- components/templates/semantic/input/text-input/renderer.js
+++ components/templates/semantic/input/text-input/renderer.js
@@ -6,5 +6,8 @@ exports.renderer = function(input, out) {
     input.loading = input.loading ? 'loading' : '';
     input.error = input.error ? 'error' : '';
     input.hasIcon = input.icon ? 'icon' : '';
+    input.hasAction = input.action ? 'action' : '';    
+    input.label = input.label ? 'labeled' : '';
+
     template.render(input, out);
 };
