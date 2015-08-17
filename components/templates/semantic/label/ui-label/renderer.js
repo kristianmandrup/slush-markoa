12d331e209917fa18f3c6b3caacef0fb4a7ab076
Kristian Mandrup <kmandrup@gmail.com>
2015-08-17 21:00:49 +0200

    nice label

--- components/templates/semantic/label/ui-label/renderer.js
+++ components/templates/semantic/label/ui-label/renderer.js
@@ -1,5 +1,8 @@
 var template = require('./template.marko');
 
 exports.renderer = function(input, out) {
+    input.isTag = input.tag ? 'tag' : '';
+    input.isRibbon = input.ribbon ? 'ribbon' : '';
+    input.isAttached = input.attached ? 'attached' : '';    
     template.render(input, out);
 };
