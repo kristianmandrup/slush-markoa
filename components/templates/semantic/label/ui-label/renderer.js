81719d0d046cfa74c1354d1c86ea990515e4b8b3
Kristian Mandrup <kmandrup@gmail.com>
2015-08-18 22:32:30 +0200

    major update and cleanup

--- components/templates/semantic/label/ui-label/renderer.js
+++ components/templates/semantic/label/ui-label/renderer.js
@@ -1,10 +1,5 @@
 var template = require('./template.marko');
 
 exports.renderer = function(input, out) {
-    input.isTag = input.tag ? 'tag' : '';
-    input.isRibbon = input.ribbon ? 'ribbon' : '';
-    input.isAttached = input.attached ? 'attached' : '';
-    input.float = input.float ? 'floating' : '';
-    input.circular = input.circular ? 'circular' : '';
     template.render(input, out);
 };
